ca00716374e6d435faf3df4afc9838b6ecf922ab
codeee18 <lint4ngboy@gmail.com>
2025-08-28 14:05:00 +0700

    done

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dunar/',
+  base: '/dan2025/',
   build: {
     rollupOptions: {
       output: {
