ec60365dfbe72edbd5b2f8fefc4f5da6ccd5b1e3
codeee18 <lint4ngboy@gmail.com>
2025-10-02 19:39:51 +0700

    done

---
+++ src/declaration.d.ts
@@ -0,0 +1,5 @@
+declare module "*.JPG";
+declare module "*.jpg";
+declare module "*.PNG";
+declare module "*.png";
+declare module "*.webp";
--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/dan2025/',
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
           framerMotion: ['framer-motion'],
-          reactIcons: ['react-icons'],	
+          reactIcons: ['react-icons'],
         }
       }
     }
   },
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
 })
