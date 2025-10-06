890d347affbd06eb1b884788b527acef6536ad10
aishareissani <aishareissani95@gmail.com>
2025-10-06 11:38:21 +0700

    update foto divisi 2

--- src/Components/Division.tsx
+++ src/Components/Division.tsx
@@ -19,7 +19,7 @@ import Makeup from "../assets/makeup.png";
 import Fm from "../assets/fm.png";
 import Pic from "../assets/pic.png";
 
-import unknown from "../assets/unknown.jpg";
+// import unknown from "../assets/unknown.jpg";
 
 import LintangModal from "../assets/Lintang.JPG";
 import VasselModal from "../assets/Vassel.jpg";
